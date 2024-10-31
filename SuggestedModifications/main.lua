524c07f572bc8595e2a98c756a27a415a7853f76
Promethibot <promethibot@gmail.com>
2024-10-31 10:20:49 -0400

    main.lua adventure editor

---
+++ SuggestedModifications/main.lua
@@ -0,0 +1,3 @@
+-- The following code tricks the editor into thinking we are the developers, unlocking the entire adventure editor suite
+-- Code should replace app.developerMode = daisy.isDeveloperModeEnabled()
+app.developerMode = true
