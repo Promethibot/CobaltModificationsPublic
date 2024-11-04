f358feb0a3dc24a9f0deee9b9dd98e1d2a1daf09
Promethibot <promethibot@gmail.com>
2024-11-04 15:39:11 -0500

    Update modeStory.lua

--- bugfixes/modeStory.lua
+++ bugfixes/modeStory.lua
@@ -5,11 +5,10 @@ editPopulateOptionalTags = function(def, tags)
 		local advs = resources.getAllIndexed("adventure")
 		
 		for adv_name, adv in pairs(advs) do
-		-- Printing out the adv_names reveals double the actual amount in the adventures folder
-		-- Might be some sort of issue involving duplication, thus causing a legitimate out of memory error?
-
-		-- It looks like this and the resulting if statement cause the crash
-			local flags = adv:getIndexes("textTag")
+		
+		-- NON-DEVELOPER EDIT
+		if not states.get("edit") then -- Crash only happens if you're trying to view it in the editor
+			local flags = adv:getIndexes("textTag") -- culprit? memoryLeak when enabled
 			
 			if flags then
 				for text_tag_name, flag in pairs(flags) do
@@ -19,7 +18,9 @@ editPopulateOptionalTags = function(def, tags)
 				end
 			end
 		end
+		-- END NON-DEVELOPER EDIT
+		end
 	end ,
 
 -- The above code seems responsible for a memory leak when you try to open a map with the gamemode Story in the editor when there is an adventures folder with stuff inside it.
--- Certain names will be missing from story mode such as Rescue George for the deed in chapter 1 without this, it seems; must test further
+-- Certain names are missing from story mode such as Rescue George for the deed in chapter 1, with or without this change; still need to test other tags like logs
