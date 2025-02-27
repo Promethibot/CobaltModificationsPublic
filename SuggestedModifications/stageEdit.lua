f9d9c3c692285266966c4651400726a1c09f1ce0
Promethibot <promethibot@gmail.com>
2025-02-26 21:32:31 -0500

    Added additional comment
    Added comment explaining a menu option could be implemented for more user customization.

--- SuggestedModifications/stageEdit.lua
+++ SuggestedModifications/stageEdit.lua
@@ -5,7 +5,8 @@
 			if self.autoBackups then
 			-- Check is redundant since saveBackups also checks but feels weird without it
 			-- Set to 120 seconds for 2 minutes
-			-- Might work best with a global or config, especially since each editor seems to have its own autoBackups variable
+			-- Might work best with a global or config, especially since each editor seems to have its own autoBackups variable.
+			-- Could add a menu option that asks for minutes and multiplies the given response by 60 for custom save intervals.
 				if (os.clock() % 120 == 0) then
 					self:saveBackups()
 					print("Backup saved!")
